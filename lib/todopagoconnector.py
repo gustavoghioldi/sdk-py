caf666864076652b9a32b8e87a5714c88f992c9c
gustavoghioldi <gustavoghioldi@gmail.com>
2015-02-02 10:38:44 -0300

    esta es una version estable

--- lib/todopagoconnector.py
+++ lib/todopagoconnector.py
@@ -1,77 +1,74 @@
+#pip install suds-jurko
 from suds.client import Client
 
-###########################################
-diccionario = 
-{
-	'Merchant':'305',
-	'MERCHANT': '305',
-	'Security': '1234567890ABCDEF1234567890ABCDEF',
-	'EncodingMethod': 'XML',
-	'OPERATIONID':'982379847',
-	'CURRENCYCODE':'032',
-	'AMOUNT':'1234'
-}
 #############################################
 
-#client = Client('http://localhost:8280/services/Authorize?wsdl', location='http://localhost:8280/services/Authorize', cache=None)
-#list_of_methods = [method for method in client.wsdl.services[0].ports[0].methods]
-
-#print list_of_methods
-
-#response = client.service.SendAuthorizeRequest(Security = '1234567890ABCDEF1234567890ABCDEF',
- #Payload="<request></request>")
-
-#print response
-#print response
-
-
 class TodoPagoConnector:
-	def __init__(self, authorization, wsdl, end_point):
-		self._authorization = authorization
-		self._wsdl = wsdl
+	def __init__(self, http_header, wsdls, end_point):
+		self._http_header = http_header
+		self._wsdls = wsdls
 		self._end_point = end_point
 	
 	######################################################################################	
 	###Methodo publico que llama a la primera funcion del servicio SendAuthorizeRequest###
 	######################################################################################
-	def sendAuthorizeRequest(self, optionsAuthorize):
+	def sendAuthorizeRequest(self, options_comercio, options_operacion):
 
-		payload = self._get_payload(optionsAuthorize);
-		self._getClientSoap()
-		return self.cliente.service.SendAuthorizeRequest(
-			Security = optionsAuthorize['Security'],
- 			Merchant = optionsAuthorize['Merchant'],
- 			EncodingMethod = 'XML',
- 			Payload=payload)
+		payload = self._get_payload(options_operacion);
+		options_comercio['Payload'] = payload
+		xml = self._parse_to_service(options_comercio, 'SendAuthorizeRequest')
+		self._getClientSoap('Authorize')
+		return self.cliente.service.SendAuthorizeRequest(__inject={'msg': xml})
 
 
 	#####################################################################################	
 	###Methodo publico que llama a la segunda funcion del servicio GetAuthorizeRequest###
 	#####################################################################################
-	def getAuthorizeRequest(self, optionsAnwser):
-		self._getClientSoap()
-		return self.cliente.service.GetAuthorizeRequest(
-			Security = optionsAnwser['Security'],
-			Merchant = optionsAnwser['Merchant'],
-			RequestKey = optionsAnwser['RequestKey'],
-			AnswerKey = optionsAnwser['AnswerKey']
-			)
+	def getAuthorizeAnswer(self, optionsAnwser):
+		self._getClientSoap('Authorize')
+		xml = self._parse_to_service(optionsAnwser, 'GetAuthorizeAnswer')
+		return self.cliente.service.GetAuthorizeAnswer(__inject={'msg': xml})
 	
 	################################################################
 	###Methodo publico que descubre todas las promociones de pago###
 	################################################################
-	def getAllPaymentMethods(self):
-		self._getClientSoap()#no va a funcionar por que cambia el wsdl y el end point	
-		return  self.cliente.service.GetAll();
+	def getAllPaymentMethods(self, optionsGAPM):
+		self._getClientSoap('PaymentMethods')
+		xml = self._parse_to_service(optionsGAPM, 'GetAll')
+		return self.cliente.service.GetAll(__inject={'msg': xml})
 	
-	########################	
+	################################################################
+	###Methodo publico que descubre todas las promociones de pago###
+	################################################################
+	def getStatus(self, optionsGS):
+		self._getClientSoap('Operations')
+		xml = self._parse_to_service(optionsGS, 'GetByOperationId')
+		return self.cliente.service.GetByOperationId(__inject={'msg': xml})
+	
+	########################
 	###Metodos privados ####
 	########################
-	def _getClientSoap(self):
-	self.cliente =  Client(self._wsdl, 
-			location=self._end_point,
-			headers={'Authorization': self._authorization}, 
+	def _parse_to_service(self, data, servicio):
+		retorno = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:api="http://api.todopago.com.ar"><soapenv:Header/><soapenv:Body>"""
+		retorno += "<api:"+servicio+">"
+		for key in data:
+			retorno += "<api:"+key+">"+data[key]+"</api:"+key+">"
+		retorno += "</api:"+servicio+">"
+		retorno += """</soapenv:Body></soapenv:Envelope>"""
+		return retorno
+
+	def _getClientSoap(self, operacion):
+		self.cliente =  Client(self._wsdls[operacion], #se tiene que extraer de un array
+			location=self._end_point+operacion, #se tiene que aprmar segun la funcion
+			headers=self._http_header, 
 			cache=None)		
+	
+	def _client_soap_header(self, data):
+		retorno = "{"
+		for key in data:
+			retorno += key+" : '"+data[key]+"', "
+		retorno += "}, "
+		return retorno
 
 	def _get_payload(self,diccionario):
 		xmlpayload = "<Request>"
@@ -79,26 +76,3 @@ class TodoPagoConnector:
 			xmlpayload += "<"+key+">"+diccionario[key]+"</"+key+">"
 		xmlpayload += "</Request>"
 		return xmlpayload
-
-###este es el ejemplo
-dicionario = {
-'Merchant':'305',
-'MERCHANT': '305',
-'Security': '1234567890ABCDEF1234567890ABCDEF',
-'EncodingMethod': 'XML',
-'OPERATIONID':'98237984733',
-'CURRENCYCODE':'032',
-'AMOUNT':'1234'
-}
-
-http_header = {'Authorization':'PRISMA 912EC803B2CE49E4A541068D495AB570'}
-wsdls = {"Authorization":""}
-
-try:
-	tpc = TodoPagoConnector
-	('PRISMA 912EC803B2CE49E4A541068D495AB570',
-	 'http://localhost:8280/services/Authorize?wsdl',
-	 'http://localhost:8280/services/Authorize')
-	tpc.sendAuthorizeRequest(diccionario)
-except:
-	print ("no se puede conectar")
